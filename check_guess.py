9045fd59769da9a5b44fe02cae0a99176f809493
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-12-10 20:49:56 -0600

    backend logic complete including unit tests

--- check_guess.py
+++ check_guess.py
@@ -6,8 +6,22 @@ class CheckGuess:
         self.shuffled_word_list = shuffled_word_list
 
     def check_user_guess(self):
-        for i in range(len(self.user_guess_list)):
-            if self.user_guess_list[i].sort() == self.shuffled_word_list[i].sort():
+        # does not work, need to sort each word in the list then compare
+    #    potential_matches = ''.join(sorted(self.user_guess_list))
+    #    potential_shuffled_matches = ''.join(sorted(self.shuffled_word_list))
+
+       for i in range(len(self.user_guess_list)):
+            potential_matches = ''.join(sorted(self.user_guess_list[i]))
+            potential_shuffled_matches = ''.join(sorted(self.shuffled_word_list[i]))
+            
+            if potential_matches == potential_shuffled_matches:
                 return True
             else:
                 return False
+
+# if __name__ == '__main__':
+#     input = ["pawn", "king", "queen"]
+#     word_list = ["pwn", "nikg", "eueuq"]
+
+#     check_guess = CheckGuess(input, word_list)
+#     print(check_guess.check_user_guess())
--- check_guess_test.py
+++ check_guess_test.py
@@ -3,17 +3,22 @@ import unittest
 from check_guess import CheckGuess
 
 class TestCheckGuess(unittest.TestCase):
-    user_guess_list = [['cda', 'btrf', 'cfr'], ['dcd', 'ete', 'fgsefg']]
-    shuffled_word_list = [['dca', 'tbfr', 'crf'], ['cdd', 'tee', 'gsfefg']]
+    user_guess_list = ['pawn', 'king', 'queen']
+    shuffled_word_list = ['wpna', 'nigk', 'eneuq']
+
+    def setUp(self):
+        self.check_guess = CheckGuess(self.user_guess_list, self.shuffled_word_list)
+
+    def tearDown(self):
+        del self.check_guess
 
     def test_object_created(self):
-        check_guess = CheckGuess(self.user_guess_list, self.shuffled_word_list)
-        assert check_guess.user_guess_list == self.user_guess_list
-        assert check_guess.shuffled_word_list == self.shuffled_word_list
+        assert self.check_guess.user_guess_list == self.user_guess_list
+        assert self.check_guess.shuffled_word_list == self.shuffled_word_list
 
     def test_check_user_guess(self):
-        check_guess = CheckGuess(self.user_guess_list, self.shuffled_word_list)
-        assert check_guess.check_user_guess() == True
+        assert self.check_guess.check_user_guess() == True
+        
 
 if __name__ == '__main__':
     unittest.main()
