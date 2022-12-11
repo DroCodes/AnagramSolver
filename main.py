4b32d745effff445e4dfcf3c6865856f27201629
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-12-11 16:05:56 -0600

    added words.text to pull random words from and cleaned up ui

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import random
 from check_guess import CheckGuess
 from start_game import StartGame
 
 import tkinter
-# TODO - clean up ui, add more words and have the words randomly selected
+# # TODO - clean up ui, add more words and have the words randomly selected
 
-possible_words = ['king', 'aardvark', 'trailer', 'python', 'computer', 'science', 'programming', 'mathematics', 'player', 'condition']
+def select_words():
+    """Selects words from the possible_words list and returns a list of 10 words"""
+    word_file = open("words.txt", "r")
+    possible_words = word_file.read().splitlines()
+    
+    selected_words = random.sample(possible_words, 10)
+    print(selected_words)
+    return selected_words
 
-initialize_game = StartGame(possible_words)
+initialize_game = StartGame(select_words())
 
 def replay():
-    initialize_game = StartGame(possible_words)
+    """Resets the game"""
+    initialize_game = StartGame(select_words())
     # reset labels
     label1.config(text=initialize_game.shuffled_word_list[0])
     label2.config(text=initialize_game.shuffled_word_list[1])
@@ -35,6 +44,7 @@ def replay():
     user_input10.delete(0, 'end')
 
 def submit_guess():
+    """Submits the user's guess and checks if it is correct"""
     user_guess = [user_input1.get(), user_input2.get(), user_input3.get(), user_input4.get(), user_input5.get(), user_input6.get(), user_input7.get(), user_input8.get(), user_input9.get(), user_input10.get()]
 
     check_guess = CheckGuess(user_guess, initialize_game.shuffled_word_list)
@@ -50,6 +60,8 @@ def create_popup(msg):
     """Creates a popup window when the user wins"""
     popup = tkinter.Tk()
     popup.wm_title(msg)
+    popup.geometry("%dx%d+%d+%d" % (200, 100, x, y))
+
     label = tkinter.Label(popup, text=msg)
     label.pack(side="top", fill="x", pady=10)
     B1 = tkinter.Button(popup, text="Okay", command = popup.destroy)
@@ -57,79 +69,88 @@ def create_popup(msg):
 
 main = tkinter.Tk()
 main.title("Word Scramble!")
-main.geometry("750x750")
+main.geometry("600x600")
 
-label1 = tkinter.Label(main, text=initialize_game.shuffled_word_list[0])
-label1.grid(row=0, column=0, columnspan=6)
+# gets screen dimensions
+screen_width = main.winfo_screenwidth()
+screen_height = main.winfo_screenheight()
 
-label2 = tkinter.Label(main, text=initialize_game.shuffled_word_list[1])
-label2.grid(row=1, column=0, columnspan=6)
+# center the window
+x = (screen_width/2) - (600/2)
+y = (screen_height/2) - (600/2)
+main.geometry("%dx%d+%d+%d" % (600, 600, x, y))
 
-label3 = tkinter.Label(main, text=initialize_game.shuffled_word_list[2])
-label3.grid(row=2, column=0, columnspan=6)
+# start game button
+start_game_button = tkinter.Button(main, width=80, padx="10", text="Start Game", command=replay)
+start_game_button.grid(row=0, column=0, columnspan=10)
 
-label4 = tkinter.Label(main, text=initialize_game.shuffled_word_list[3])
-label4.grid(row=3, column=0, columnspan=6)
+label1 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[0])
+label1.grid(row=1, column=0, columnspan=5)
 
-label5 = tkinter.Label(main, text=initialize_game.shuffled_word_list[4])
-label5.grid(row=4, column=0, columnspan=6)
+label2 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[1])
+label2.grid(row=2, column=0, columnspan=5)
 
-label6 = tkinter.Label(main, text=initialize_game.shuffled_word_list[5])
-label6.grid(row=5, column=0, columnspan=6)
+label3 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[2])
+label3.grid(row=3, column=0, columnspan=5)
 
-label7 = tkinter.Label(main, text=initialize_game.shuffled_word_list[6])
-label7.grid(row=6, column=0, columnspan=6)
+label4 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[3])
+label4.grid(row=4, column=0, columnspan=5)
 
-label8 = tkinter.Label(main, text=initialize_game.shuffled_word_list[7])
-label8.grid(row=7, column=0, columnspan=6)
+label5 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[4])
+label5.grid(row=5, column=0, columnspan=5)
 
-label9 = tkinter.Label(main, text=initialize_game.shuffled_word_list[8])
-label9.grid(row=8, column=0, columnspan=6)
+label6 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[5])
+label6.grid(row=6, column=0, columnspan=5)
 
-label10 = tkinter.Label(main, text=initialize_game.shuffled_word_list[9])
-label10.grid(row=9, column=0, columnspan=6)
+label7 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[6])
+label7.grid(row=7, column=0, columnspan=5)
+
+label8 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[7])
+label8.grid(row=8, column=0, columnspan=5)
+
+label9 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[8])
+label9.grid(row=9, column=0, columnspan=5)
+
+label10 = tkinter.Label(main, width=40, pady=10, text=initialize_game.shuffled_word_list[9])
+label10.grid(row=10, column=0, columnspan=5)
 
 # user input
 user_input1 = tkinter.Entry(main)
-user_input1.grid(row=0, column=7, columnspan=6)
+user_input1.grid(row=1, column=7, columnspan=6)
 
 user_input2 = tkinter.Entry(main)
-user_input2.grid(row=1, column=7, columnspan=6)
+user_input2.grid(row=2, column=7, columnspan=6)
 
 user_input3 = tkinter.Entry(main)
-user_input3.grid(row=2, column=7, columnspan=6)
+user_input3.grid(row=3, column=7, columnspan=6)
 
 user_input4 = tkinter.Entry(main)
-user_input4.grid(row=3, column=7, columnspan=6)
+user_input4.grid(row=4, column=7, columnspan=6)
 
 user_input5 = tkinter.Entry(main)
-user_input5.grid(row=4, column=7, columnspan=6)
+user_input5.grid(row=5, column=7, columnspan=6)
 
 user_input6 = tkinter.Entry(main)
-user_input6.grid(row=5, column=7, columnspan=6)
+user_input6.grid(row=6, column=7, columnspan=6)
 
 user_input7 = tkinter.Entry(main)
-user_input7.grid(row=6, column=7, columnspan=6)
+user_input7.grid(row=7, column=7, columnspan=6)
 
 user_input8 = tkinter.Entry(main)
-user_input8.grid(row=7, column=7, columnspan=6)
+user_input8.grid(row=8, column=7, columnspan=6)
 
 user_input9 = tkinter.Entry(main)
-user_input9.grid(row=8, column=7, columnspan=6)
+user_input9.grid(row=9, column=7, columnspan=6)
 
 user_input10 = tkinter.Entry(main)
-user_input10.grid(row=9, column=7, columnspan=6)
-
-# start game button
-start_game_button = tkinter.Button(main, text="Start Game", command=replay)
-start_game_button.grid(row=10, column=0, columnspan=6)
+user_input10.grid(row=10, column=7, columnspan=6)
 
 # check guess button
-check_guess_button = tkinter.Button(main, text="Check Guess", command=submit_guess)
-check_guess_button.grid(row=10, column=7, columnspan=6)
+check_guess_button = tkinter.Button(main, width=20, bg="#90EE90", text="Check Guess", command=submit_guess)
+check_guess_button.grid(row=11, column=0, columnspan=5)
 
 # quit button
-quit_button = tkinter.Button(main, text="Quit", command=main.destroy)
-quit_button.grid(row=11, column=0, columnspan=6)
+quit_button = tkinter.Button(main, text="Quit", width=20, bg="red", command=main.destroy)
+quit_button.grid(row=11, column=7, columnspan=5)
 
 main.mainloop()
